ff13e8152148883bc927997ab9d1a7ab4cb5404f
Valen <gg172138406@gmail.com>
2024-02-29 23:02:30 +0800

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import React, { useRef } from "react";
-import Footer from "./components/footer/Footer";
 import About from "./components/about/About";
 import Products from "./components/products/Products";
 import Services from "./components/services/Services";
 import News from "./components/news/News";
 import Navbar from "./components/navbar/Navbar";
-
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
